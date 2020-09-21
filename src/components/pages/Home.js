b82ef3bf269385494f649c66f07d8d26567938d8
sangam31 <66167638+sangam31@users.noreply.github.com>
2020-09-22 03:13:17 +0530

    Footer and CSS

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import '../../App.css'
 import React from 'react'
 import HeroSection from '../HeroSection'
 import Cards from '../Cards'
+import Footer from '../Footer'
 
 function Home () {
     return(
     <>
        <HeroSection />
        <Cards />
+       <Footer/>
     </>
     )
 }
